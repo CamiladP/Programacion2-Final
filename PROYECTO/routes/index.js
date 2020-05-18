1dd3b5636e21a495504159ab896332259ea733ec
Cami <mdepaul@udesa.edu.ar>
2020-05-18 18:17:32 -0300

    Rutas y controladores, acceso a HOME

--- PROYECTO/routes/index.js
+++ PROYECTO/routes/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
+let controllerSerion = require("../controllers/controllerSerion")
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', controllerSerion.index )
 
 module.exports = router;
