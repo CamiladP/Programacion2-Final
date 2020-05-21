c0163a748e6ecce14a84159e9e18b20b54b1f1ac
Lola Marotta <lmarotta@udesa.edu.ar>
2020-05-21 20:18:40 -0300

    Base de datos en vsc

--- PROYECTO/controllers/controllerSerion.js
+++ PROYECTO/controllers/controllerSerion.js
@@ -1,9 +1,18 @@
+let db = require("../db/models");
+
 let controllerSerion = {
 
     index: function(req,res){
 
         res.render("home")
 
+    },
+
+    prueba: function(req, res) {
+        db.sequelize.query("SELECT * FROM usuarios")
+        .then(function(response) {
+            res.send(response)
+        })
     }
 
 
--- PROYECTO/routes/index.js
+++ PROYECTO/routes/index.js
@@ -3,6 +3,8 @@ var router = express.Router();
 let controllerSerion = require("../controllers/controllerSerion")
 
 /* GET home page. */
-router.get('/', controllerSerion.index )
+router.get('/', controllerSerion.index );
+
+router.get('/prueba', controllerSerion.prueba)
 
 module.exports = router;
