e1a4b55b12659700fe8c6f3ea1556d3e554539c7
Lola Marotta <lmarotta@udesa.edu.ar>
2020-05-21 20:44:38 -0300

    modulos usuarios y resenias

---
+++ PROYECTO/db/models/Resenias.js
@@ -0,0 +1,25 @@
+module.exports = (sequelize, dataTypes) => {
+
+    let cols = {
+        id: {
+            type: dataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        iderie: {type: dataTypes.INTEGER},
+        idusuario: {type: dataTypes.INTEGER, foreignKey: true},
+        texto: {type: dataTypes.STRING},
+        fechacreacion: {type: dataTypes.DATE},
+        fechaactualizacion: {type: dataTypes.DATE},
+        puntaje: {type: dataTypes.INTEGER},
+    };
+
+    let config = {
+        tableName: "resenias",
+        timestamps: true,
+    };
+
+    const resenias = sequelize.define("resenias", cols, config);
+    return resenias;
+
+};
---
+++ PROYECTO/db/models/Usuarios.js
@@ -0,0 +1,23 @@
+module.exports = (sequelize, dataTypes) => {
+
+    let cols = {
+        id: {
+            type: dataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        nombre: {type: dataTypes.STRING}, 
+        email: {type: dataTypes.STRING}, 
+        password: {type: dataTypes.STRING},
+        nacimiento: {type: dataTypes.DATE}
+    };
+
+    let config = {
+        tableName: "usuarios",
+        timestamps: true,
+    };
+
+    const usuarios = sequelize.define("usuarios", cols, config);
+    return usuarios;
+
+};
